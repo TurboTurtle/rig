d6944798aace66e83c31a930c3efca2f961d2a0d
Jake Hunsaker <jhunsake@redhat.com>
2019-02-04 16:10:45 -0500

    [PEP8] PEP8 fixes
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- rigging/__init__.py
+++ rigging/__init__.py
@@ -37,9 +37,9 @@ class Rigging():
     def __init__(self, parser, args):
         # Add the common rigging options here
         parser.add_argument('--foreground', action='store_true', default=False,
-                        help='Run the rig in the foreground')
+                            help='Run the rig in the foreground')
         parser.add_argument('--debug', action='store_true',
-                        help='Print debug messages to console')
+                            help='Print debug messages to console')
         self.parser = parser
         self.args = args
 
