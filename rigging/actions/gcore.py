d266f392d466ad128d4f62813eef6adfce23b415
Jake Hunsaker <jhunsake@redhat.com>
2019-07-18 14:20:23 -0400

    [gcore] Update action to use get_option() method
    Updates the gcore action to use the new get_option() method and to also
    use the process rig's watched pid(s) in the event the '--gcore' option
    is not given an explicit value.
    
    Also improved the function to convert commandline values to pids and
    checks to make sure the pid(s) exist.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- rigging/actions/gcore.py
+++ rigging/actions/gcore.py
@@ -9,6 +9,8 @@
 # See the LICENSE file in the source distribution for further information.
 
 from rigging.actions import BaseAction
+from os.path import basename
+import psutil
 
 
 class Gcore(BaseAction):
@@ -23,13 +25,31 @@ class Gcore(BaseAction):
     required_binaries = ('gcore',)
 
     def add_action_options(self, parser):
-        parser.add_argument('--gcore', action='append',
+        parser.add_argument('--gcore', nargs='?', action='append', default=[],
                             help=self.enabling_opt_desc)
         parser.add_argument('--all-pids', action='store_true',
                             help=('Execute over all pids found when using '
                                   'process names')
                             )
         return parser
+
+    def _get_pid_from_name(self, pname):
+        '''
+        Find the PID(s) associated with the given process name
+        '''
+        _procs = []
+        filt = ['name', 'exe', 'cmdline', 'pid']
+        for proc in psutil.process_iter(attrs=filt):
+            if (proc.info['name'] == pname or
+                    proc.info['exe'] and basename(proc.info['exe']) == pname or
+                    proc.info['cmdline'] and proc.info['cmdline'][0] == pname):
+                _procs.append(proc.info['pid'])
+        if len(_procs) > 1 and not self.get_option('all'):
+            msg = ("Multiple PIDs found for process '%s', use --all to watch "
+                   "all PIDs" % pname)
+            self.log_error(msg)
+            raise Exception
+        return _procs
 
     def pre_action(self):
         '''
@@ -39,25 +59,33 @@ class Gcore(BaseAction):
         name is provided, the core name will be core.$name.$pid
         '''
         self.pid_list = []
-        for pid in self.args['gcore']:
+        # the collected list of requested pids, to be filled later
+        procs = []
+        # the raw commandline value(s) for --gcore
+        _pid = self.get_option('gcore')
+        for _p in _pid:
+            if _p is None:
+                if not self.get_option('process'):
+                    msg = ('gcore action must be given a pid or process name, '
+                           'or be used with the \'process\' rig type for no'
+                           'value')
+                    self.log_error(msg)
+                    return False
+                procs.extend(self.get_option('process'))
+            else:
+                procs.extend(_p.split(','))
+        # actually verify the pids derived from the command line options
+        for pid in procs:
             try:
                 self.pid_list.append((int(pid), ''))
             except ValueError:
                 # we were handed a process name, get the pid of it
-                ret = self.exec_cmd("pidof %s" % pid)
-                if ret['status'] == 0:
-                    pids = ret['stdout'].split()
-                    if len(pids) > 1 and not self.args['all_pids']:
-                        err = ("Multiple pids found for process %s. Use "
-                               "--all-pids or specify a pid manually")
-                        self.log_error(err % pid)
-                        return False
-                    for proc in pids:
-                        self.pid_list.append((int(proc), pid))
-                    return True
-                else:
-                    self.log_error("Could not find PID(s) for '%s'" % pid)
-                    return False
+                proc_pids = self._get_pid_from_name(pid)
+                for proc in proc_pids:
+                    self.pid_list.append((int(proc), pid))
+        msg = ("Determined pid list to collect coredumps for to be: %s"
+               % ','.join(str(p[0]) for p in self.pid_list))
+        self.log_debug(msg)
         return True
 
     def trigger_action(self):
@@ -65,6 +93,10 @@ class Gcore(BaseAction):
             loc = self.tmp_dir + 'core'
             if pid[1]:
                 loc += ".%s" % pid[1]
+            if not psutil.pid_exists(int(pid[0])):
+                self.log_error("Cannot collect core for pid %s - pid no "
+                               "longer exists" % pid[0])
+                continue
             self.log_debug("Collecting gcore of %s at %s" % (pid[0], loc))
             try:
                 ret = self.exec_cmd("gcore -o %s %s" % (loc, pid[0]))
