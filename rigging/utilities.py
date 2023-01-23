e90150eb1a890a712d2b245acb54993c63aca9d7
Jake Hunsaker <jhunsake@redhat.com>
2023-01-23 14:10:37 -0500

    [rig] Set initial framework for new command structure
    This commit lays the groundwork for a new approach to rig CLI commands,
    e.g. a user running `rig info`.
    
    This new approach abstracts the handling of command strings compared to
    the current method, giving each command its own subparser for which to
    control its options. In this way, we will no longer need to account for
    colliding option names.
    
    Note that this is a stepping stone commit, and rig is not considered
    functional at this specific point in time as we transition to the new
    2.0 design. Future commits will rectify this.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- rigging/utilities.py
+++ rigging/utilities.py
@@ -11,12 +11,18 @@
 import inspect
 import os
 
+from rigging.commands import RigCmd
 from rigging.rigs import BaseRig
 
 
-def import_modules(modname):
+def import_modules(modname, subclass):
     """
     Import helper to import all classes from a rig definition.
+
+    :param modname: The module name to inspect
+    :param subclass: The subclass that we should match found entities on
+
+    :return: A list of discovered modules
     """
     mod_short_name = modname.split('.')[2]
     module = __import__(modname, globals(), locals(), [mod_short_name])
@@ -26,12 +32,35 @@ def import_modules(modname):
         if not isinstance(mod, list):
             mod = [mod]
         for _mod in mod:
-            if _mod[0] in ('Rigging', 'BaseRig'):
+            if _mod[0] in ('Rigging', subclass.__name__):
                 continue
-            if not issubclass(_mod[1], BaseRig):
+            if not issubclass(_mod[1], subclass):
                 continue
             modules.append(_mod)
     return modules
+
+
+def find_modules(pkg, pkgstr, subclass):
+    modules = []
+    for path in pkg.__path__:
+        if os.path.isdir(path):
+            for pyfile in sorted(os.listdir(path)):
+                if not pyfile.endswith('.py') or '__' in pyfile:
+                    continue
+                fname, ext = os.path.splitext(pyfile)
+                _mod = f"{pkgstr}.{fname}"
+                modules.extend(import_modules(_mod, subclass))
+    return modules
+
+
+def load_rig_commands():
+    import rigging.commands
+    cmds = rigging.commands
+    rig_cmds = {}
+    modules = find_modules(cmds, 'rigging.commands', RigCmd)
+    for mod in modules:
+        rig_cmds[mod[0].lower().rstrip('cmd')] = mod[1]
+    return rig_cmds
 
 
 def load_supported_rigs():
@@ -44,15 +73,8 @@ def load_supported_rigs():
     import rigging.rigs
     monitors = rigging.rigs
     _supported_rigs = {}
-    modules = []
-    for path in monitors.__path__:
-        if os.path.isdir(path):
-            for pyfile in sorted(os.listdir(path)):
-                if not pyfile.endswith('.py') or '__' in pyfile:
-                    continue
-                fname, ext = os.path.splitext(pyfile)
-                _mod = "rigging.rigs.%s" % fname
-                modules.extend(import_modules(_mod))
+    modules = find_modules(monitors, 'rigging.rigs', BaseRig)
     for mod in modules:
         _supported_rigs[mod[0].lower()] = mod[1]
     return _supported_rigs
+
