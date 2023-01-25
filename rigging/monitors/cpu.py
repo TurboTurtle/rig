ac716de4f8e46845876a5b185c80d9b87c7695e5
Jake Hunsaker <jhunsake@redhat.com>
2023-01-25 15:09:04 -0500

    [system] Convert system rig to new style of monitors
    This commit updates the previous `system` rig to the new `monitors`
    design, and in so doing splits the rig into 3 distinct monitors. Whereas
    the system rig handle all the following before, the new monitors are:
    
      + system - handles system temperature and load
      + memory - handles memory usage monitoring
      + cpu    - handles CPU usage monitoring
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ rigging/monitors/cpu.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2023 Red Hat, Inc., Jake Hunsaker <jhunsake@redhat.com>
+
+# This file is part of the rig project: https://github.com/TurboTurtle/rig
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information
+
+import psutil
+
+from rigging.monitors import BaseMonitor
+
+
+class CpuMonitor(BaseMonitor):
+    """
+    Monitors for various CPU utilization metrics such as overall use, time
+    spent in iowait/steal, etc...
+
+    Users may specify any or all of the metrics reported by psutil, aside from
+    idle, or may monitor the overall CPU usage.
+    """
+
+    monitor_name = 'cpu'
+
+    def configure(self, percent=None, iowait=None, steal=None, system=None,
+                  nice=None, guest=None, guest_nice=None, user=None):
+        """
+        :param percent: CPU usage threshold expressed as a percentage
+        :param iowait: Threshold for percent of time spent in iowait
+        :param steal: Threshold for percent of time spent in steal
+        :param system: Threshold for percent of time spent in system
+        :param nice: Threshold for the percent of time spent in nice
+        :param guest: Threshold for the percent of time spent in guest
+        :param guest_nice: Threshold for the percent of time spent in
+                           guest_nice
+        :param user: Threshold for percent of time spent in user
+        """
+        _metrics = {
+            'percent': percent,
+            'iowait': iowait,
+            'steal': steal,
+            'system': system,
+            'nice': nice,
+            'guest': guest,
+            'guest_nice': guest_nice,
+            'user': user
+        }
+
+        if not any(m is not None for m in _metrics):
+            raise Exception(
+                f"Must specify at least one of "
+                f"{', '.join(m for m in _metrics.keys())}"
+            )
+
+        for _m in _metrics:
+            if _metrics[_m] is not None:
+                try:
+                    if float(_metrics[_m]) > 100:
+                        raise Exception(f"'{_m}' cannot exceed 100.")
+                except (TypeError, ValueError):
+                    raise Exception(
+                        f"'{_m}' must be integer or float. Not {_m.__class__}."
+                    )
+
+        if percent is not None:
+            self.add_monitor_thread(
+                self.watch_cpu_utilization,
+                (float(percent), )
+            )
+
+        _metrics.pop('percent')
+
+        if any(m is not None for m in _metrics.values()):
+            self.add_monitor_thread(self.watch_cpu_metrics, (_metrics, ))
+
+    def watch_cpu_utilization(self, perc):
+        """
+        Monitor the overall CPU utilization expressed as a percentage.
+
+        :param perc: Utilization rate to trigger on when met or exceeded
+        """
+        # First iteration returns meaningless data
+        psutil.cpu_percent()
+        while True:
+            _val = psutil.cpu_percent(interval=self.config['interval'])
+            if _val > perc:
+                self.logger.info(
+                    f"CPU usage at {_val}%, exceeding threshold of {perc}%"
+                )
+                return True
+
+    def watch_cpu_metrics(self, metrics):
+        """
+        Monitor specific aspects of CPU usage, such as iowait time, as a
+        percentage of overall CPU time.
+
+        Note that with this method, we monitor _all_ CPU metrics in a single
+        thread and with a single polling interval.
+
+        :param metrics: A dict of metrics and their respective thresholds
+        """
+        _monitor = {}
+        for metric in metrics:
+            if metrics[metric] is not None:
+                _monitor[metric] = float(metrics[metric])
+        # first return is usually garbage data
+        psutil.cpu_times_percent()
+        while True:
+            _poll = psutil.cpu_times_percent(self.config['interval'])
+            for _mon in _monitor:
+                _val = float(getattr(_poll, _mon))
+                if _val >= _monitor[_mon]:
+                    self.logger.info(
+                        f"CPU metric {_mon} is at {_val}%, exceeding threshold"
+                        f" of {_monitor[_mon]}%"
+                    )
+                    return True
---
+++ rigging/monitors/system.py
@@ -0,0 +1,107 @@
+# Copyright (C) 2023 Red Hat, Inc., Jake Hunsaker <jhunsake@redhat.com>
+
+# This file is part of the rig project: https://github.com/TurboTurtle/rig
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information
+
+import os
+import psutil
+
+from rigging.monitors import BaseMonitor
+
+
+class SystemMonitor(BaseMonitor):
+    """
+    Monitor overall system utilization metrics for areas such as CPU, memory,
+    disk usage, temperature, etc..
+    """
+
+    monitor_name = 'system'
+
+    def configure(self, temperature=None, loadavg=None, loadavg_interval=1):
+        """
+        :param temperature: Threshold temperature in Celsius of the CPU
+        :param loadavg: Threshold for relative system load reported by loadavg
+        :param loadavg_interval: Which loadavg minute interval to track
+        """
+        if not any([temperature, loadavg]):
+            raise Exception(
+                'Must specify at least one of \'temperature\', or \'loadavg\''
+            )
+
+        if temperature is not None:
+            self._configure_temp_monitor(temperature)
+
+        if loadavg is not None:
+            self._configure_loadavg_monitor(loadavg, loadavg_interval)
+
+    def _configure_temp_monitor(self, temperature):
+        _t = psutil.sensors_temperatures()
+        if not any(_i.endswith('temp') for _i in _t.keys()):
+            raise Exception(
+                'local hardware does not appear to report CPU temperatures'
+            )
+        try:
+            temp = int(temperature)
+        except Exception:
+            raise Exception(
+                f"'temperature' must be integer. Not "
+                f"{temperature.__class__}."
+            )
+        self.add_monitor_thread(self.watch_temperature, (temp,))
+
+    def _configure_loadavg_monitor(self, loadavg, loadavg_interval):
+        try:
+            float(loadavg)
+        except Exception:
+            raise Exception(f"'loadavg' must be integer or float. "
+                            f"Not {loadavg.__class__}.")
+        if int(loadavg_interval) not in [1, 5, 15]:
+            raise Exception(f"'loadavg_interval' must be 1, 5, or 15. "
+                            f"Not {loadavg_interval}.")
+        self.add_monitor_thread(self.watch_loadavg,
+                                (loadavg, loadavg_interval))
+
+    def watch_temperature(self, temp):
+        """
+        Monitor the system temperature and if it meets or exceeds the specified
+        temperature, trigger.
+
+        :param temp: Temperature in Celsius to trigger when met or exceeded
+        """
+        # get the key for the temperature reading
+        _t = psutil.sensors_temperatures()
+        _key = [k for k in _t.keys() if k.endswith('temp')][0]
+        while True:
+            cur_temp = psutil.sensors_temperatures()[_key][0].current
+            if cur_temp >= temp:
+                self.logger.info(
+                    f"System temperature is {cur_temp} C, exceeding threshold "
+                    f"of {temp} C"
+                )
+                return True
+            self.wait_loop()
+
+    def watch_loadavg(self, threshold, interval):
+        """
+        Monitor the ongoing system loadavg as reported by the OS.
+
+        :param threshold: loadavg value to trigger on if met or exceeded
+        :param interval: Which reporting interval to use - 1, 5, or 15 minutes
+        """
+        idx = (1, 5, 15).index(interval)
+        if isinstance(threshold, str):
+            threshold = float(threshold)
+        while True:
+            _val = round(os.getloadavg()[idx], 2)
+            if _val >= threshold:
+                self.logger.info(
+                    f"System {interval}-minute loadavg at {_val}, exceeding "
+                    f"threshold of {threshold}"
+                )
+                return True
+            self.wait_loop()
