e627d6d8b260bad5b0f66a1127619baea489c561
Jake Hunsaker <jhunsake@redhat.com>
2023-01-26 17:30:59 -0500

    [process] Port `process` rig to new monitor design
    Ports the previous `process` rig to the new monitor design for rig v2.
    The new monitor accepts either a single process or a list of processes
    specified by either PID(s) or process/command name(s). Process names
    can be exact strings or python-compatible regexes.
    
    One difference between the new monitor and the old rig, is that the
    `--all` option has not been brought forward to the new monitor. It is
    assumed that any process names given to this monitor is done to
    intentionally catch all processes matching that name.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ rigging/monitors/process.py
@@ -0,0 +1,279 @@
+# Copyright (C) 2023 Red Hat, Inc., Jake Hunsaker <jhunsake@redhat.com>
+
+# This file is part of the rig project: https://github.com/TurboTurtle/rig
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information
+
+import psutil
+import re
+
+from os.path import basename
+from rigging.monitors import BaseMonitor
+from rigging.utilities import convert_to_bytes, convert_to_human
+from rigging.exceptions import DestroyRig
+from threading import Lock
+
+PROC_STATES = {
+    psutil.STATUS_RUNNING: ('running', 'R', 'run'),
+    psutil.STATUS_DISK_SLEEP: ('disk-sleep', 'disk_sleep', 'D', 'UN',
+                               'uninterruptible', 'uninterruptible_sleep'),
+    psutil.STATUS_SLEEPING: ('sleeping', 'sleep', 'S'),
+    psutil.STATUS_STOPPED: ('stopped', 'stop', 'T'),
+    psutil.STATUS_ZOMBIE: ('zombie', 'Z')
+}
+
+
+class ProcessMonitor(BaseMonitor):
+    """
+    Watch a process for changes in process state, or resource consumption.
+    Multiple processes may be watched at a time and processes may be specified
+    by either name or PID.
+    """
+
+    monitor_name = 'process'
+
+    def configure(self, procs, cpu_percent=None, memory_percent=None,
+                  state=None, vms=None, rss=None):
+        """
+        :param procs: A process or list of process to watch. Specify either
+                      a PID or command. If command, accepts a regex.
+        :param cpu_percent: Trigger if CPU usage exceeds this threshold
+        :param memory_percent: Trigger if memory usage exceeds this threshold
+        :param state: The POSIX state of the process to trigger on
+        :param vms: The virtual memory size threshold to trigger on
+        :param rss: The resident set size threshold to trigger on
+        """
+        if state is not None:
+            if not isinstance(state, str):
+                raise Exception(f"'state' must be str, not {state}")
+            self._state_invert_match = state.startswith('!')
+        else:
+            self._state_invert_match = False
+
+        self.lock = Lock()
+
+        self.metrics = {
+            'cpu_percent': cpu_percent,
+            'memory_percent': memory_percent,
+            'state': self._get_state_code(state) if state else None,
+            'vms': convert_to_bytes(vms) if vms else None,
+            'rss': convert_to_bytes(rss) if rss else None
+        }
+
+        for perc in ['cpu_percent', 'memory_percent']:
+            if self.metrics[perc] is not None:
+                try:
+                    self.metrics[perc] = float(self.metrics[perc])
+                except Exception:
+                    raise Exception(f"'{perc}' must be integer or float, not "
+                                    f"{perc.__class__}")
+
+        if not any([m is not None for m in self.metrics.values()]):
+            raise Exception(
+                f"Must specify at least one of "
+                f"{', '.join(self.metrics.keys())}"
+            )
+
+        if isinstance(procs, str | int):
+            _procs = [procs]
+        elif isinstance(procs, list):
+            _procs = procs
+        else:
+            raise Exception(
+                f"'procs' must be string or list. Not {procs.__class__}"
+            )
+
+        self.procs = self._get_proc_pids(_procs)
+        if not self.procs:
+            raise Exception(
+                "No PIDs matching specified process identifiers found. "
+                "Aborting..."
+            )
+
+        _state = self.metrics.pop('state')
+        for proc in self.procs:
+            if _state:
+                self.add_monitor_thread(
+                    self.watch_process_for_status,
+                    (proc, _state, )
+                )
+            if any(m is not None for m in self.metrics.values()):
+                self.add_monitor_thread(
+                    self.watch_process_for_utilization,
+                    (proc, )
+                )
+
+    @staticmethod
+    def _get_state_code(state):
+        """
+        Converts the given state string to the string used by psutil. This
+        allows us to be more flexible in what users can set in their rigfiles.
+
+        :param state: The state specified in the rigfile
+        :return: A matching string from a psutil.STATUS_* constant
+        """
+        for _state_code in PROC_STATES:
+            if state.strip('!') in PROC_STATES[_state_code]:
+                return _state_code
+        raise Exception(
+            f"Unable to parse specified process status '{state}'. "
+            f"See man rig for supported values."
+        )
+
+    def watch_process_for_status(self, pid, state):
+        """
+        Monitor a specific pid for the defined status. Generally speaking, if
+        a pid _enters_ this state, we trigger.
+
+        This can be inverted if the user prefixed their rigfile `state` value
+        with `!` to signify we should be looking for "not in this state".
+        In such a case, the monitor will trigger when we first detect that the
+        pid is _not_ in the defined state.
+
+        However, in the specific case of state being `!running`, the monitor
+        accepts that a state of `sleeping` is not a reason to trigger.
+
+        :param pid: The PID of the process to monitor
+        :param state: The status string used by psutil to match against
+        """
+        self.logger.debug(f"Launching monitor thread for PID {pid}")
+        proc = psutil.Process(pid)
+        while True:
+            try:
+                _status = proc.status()
+                if (state == 'running' and _status == 'sleeping' and
+                        self._state_invert_match):
+                    pass
+                elif (((state == _status) and not self._state_invert_match) or
+                      ((state != _status) and self._state_invert_match)):
+                    self.logger.info(
+                        f"Process {pid} is in state {_status} matching trigger"
+                        f" state '{'!' if self._state_invert_match else ''}"
+                        f"{state}'"
+                    )
+                    return True
+                self.wait_loop()
+            except psutil.NoSuchProcess:
+                if state == 'running' and self._state_invert_match:
+                    self.logger.info(
+                        f"Process {pid} no longer exists, matching trigger "
+                        f"state of !{state}."
+                    )
+                    return True
+                self.logger.info(
+                    f"Process {pid} no longer exists, and desired trigger "
+                    f"state is not '!running'. Holding monitor active until "
+                    f"all pid monitoring is resolved."
+                )
+                return self._hold_thread(pid)
+            except Exception as err:
+                self.logger.info(f"Error while polling process state: {err}")
+                return self._hold_thread(pid)
+
+    @property
+    def all_pids_are_dead(self):
+        return all(not psutil.pid_exists(p) for p in self.procs)
+
+    def _hold_thread(self, pid):
+        self.logger.debug(
+            f"Process {pid} no longer exists. Holding monitor thread until all"
+            f" specified pids either die or trigger rig."
+        )
+        while not self.all_pids_are_dead:
+            with self.lock:
+                self.wait_loop()
+        raise DestroyRig(
+            'All specified pids now dead, and state not defined to trigger'
+            ' on this condition.'
+        )
+        # return False
+
+    @staticmethod
+    def _get_proc_pids(proc_list):
+        """
+        Convert any given process names/commands to relevant PIDs, as PIDs are
+        what we need to use to monitor given process(es).
+
+        :param proc_list: A list of process names, commands, or pre-determined
+                          PIDs.
+        :return: A list of all PIDs
+        """
+        _pids = []
+        filt = ['name', 'exe', 'cmdline', 'pid']
+        _running_procs = psutil.process_iter(attrs=filt)
+        for proc in proc_list:
+            if not proc:
+                continue
+            try:
+                for _each in _running_procs:
+                    try:
+                        if _each.pid == int(proc):
+                            _pids.append(int(proc))
+                    except Exception:
+                        # we have a process/command name
+                        _preg = re.compile(proc)
+                        _stats = [
+                            _each.info.get('name', ''),
+                            _each.info.get('exe', ''),
+                        ]
+                        if _each.info.get('cmdline'):
+                            _stats.append(
+                                basename(_each.info.get('cmdline')[0])
+                            )
+                        if any(_preg.match(_p) for _p in _stats if _p):
+                            _pids.append(_each.pid)
+            except Exception as err:
+                raise Exception(f"Error getting process pids: {err}")
+        _pids = list(set(_pids))
+        return _pids
+
+    def watch_process_for_utilization(self, pid):
+        """
+        Monitor a given pid for exceeding any of the utilization metrics set in
+        the rigfile.
+
+        :param pid: The PID of the process to watch
+        """
+        _metrics = {}
+        for _met in self.metrics:
+            if self.metrics[_met] is not None:
+                _metrics[_met] = self.metrics[_met]
+        _log_msg = ', '.join([
+            f"{k} above {convert_to_human(v) if 'percent' not in k else v }"
+            for k, v in _metrics.items()
+        ])
+        self.logger.debug(
+            f"Launching monitor thread for pid {pid} for "
+            f"{_log_msg}"
+        )
+
+        proc = psutil.Process(pid)
+        while True:
+            try:
+                for perc_met in ['cpu_percent', 'memory_percent']:
+                    if _metrics.get(perc_met):
+                        _metric = round(getattr(proc, perc_met)(), 2)
+                        if _metric > _metrics[perc_met]:
+                            self.logger.info(
+                                f"Process {pid} {perc_met} usage of {_metric}%"
+                                f" exceeds threshold of {_metrics[perc_met]}%"
+                            )
+                            return True
+                for mem_stat in ['vms', 'rss']:
+                    if _metrics.get(mem_stat):
+                        _memory = proc.memory_info()
+                        _stat = getattr(_memory, mem_stat)
+                        if _stat > _metrics[mem_stat]:
+                            self.logger.info(
+                                f"Process {pid} {mem_stat} usage of "
+                                f"{convert_to_human(_stat)} exceeds threshold "
+                                f"of {convert_to_human(_metrics[mem_stat])}"
+                            )
+                            return True
+                self.wait_loop()
+            except psutil.NoSuchProcess:
+                return self._hold_thread(pid)
