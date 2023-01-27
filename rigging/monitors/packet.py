0a380705e354d1fdca385706d5f3360cd044707f
Jake Hunsaker <jhunsake@redhat.com>
2023-01-27 14:16:45 -0500

    [packet] Port packet rig to new monitor design
    Ports the `packet` rig originally contributed by Juanma Sanchez to the
    new monitor design.
    
    The `ifname` option has been renamed to `interface`, which will be the
    standard naming convention for monitors and actions going forward.
    
    Of note, the `tcpflags` option now takes either a string for a single
    flag or a list of flags via the rigfile. It no longer supports parsing
    flags via the pipe character. For example, previously using something
    like `--tcpflags='SYN|ACK'` now requires the following in a rigfile:
    
    monitors:
      packet:
        interface: eth0
        tcpflags:
          - SYN
          - ACK
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- rigging/monitors/packet.py
+++ rigging/monitors/packet.py
@@ -1,5 +1,12 @@
-from rigging.rigs import BaseRig
-from rigging.exceptions import CannotConfigureRigError
+# This file is part of the rig project: https://github.com/TurboTurtle/rig
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information
+
+from rigging.monitors import BaseMonitor
 from enum import Enum, IntFlag
 from socket import socket, AF_PACKET, SOCK_RAW, \
                    IPPROTO_TCP, IPPROTO_UDP, IPPROTO_ICMP, \
@@ -10,12 +17,13 @@ import re
 
 ETH_P_ALL = 3
 ETH_IPV4 = 0x0800
-ETH_ARP  = 0x0806
+ETH_ARP = 0x0806
 ETH_VLAN = 0x8100
 ETH_IPV6 = 0x86dd
 
 SOL_PACKET = 263
 SO_ATTACH_FILTER = 26
+
 
 class PayloadMatch:
     def __init__(self, regex):
@@ -34,6 +42,7 @@ class PayloadMatch:
     def __repr__(self):
         return self._regex
 
+
 class TCP_FLAGS(IntFlag):
     FIN = 1
     SYN = 2
@@ -44,12 +53,13 @@ class TCP_FLAGS(IntFlag):
     ECN = 64
     CWR = 128
 
+
 class ICMP_TYPES(Enum):
     ECHO_REPLY = 0
     DESTINATION_UNREACHABLE = 3
     REDIRECT = 5
     ECHO = 8
-    ROUTER_ADVERTISMENT = 9
+    ROUTER_ADVERTISEMENT = 9
     ROUTER_SELECTION = 10
     TIME_EXCEEDED = 11
     PARAMETER_PROBLEM = 12
@@ -60,6 +70,7 @@ class ICMP_TYPES(Enum):
     ADDRESS_MASK_REQUEST = 17
     ADDRESS_MASK_REPLY = 18
     TRACEROUTE = 30
+
 
 class ICMP_DEST_UNREACH(Enum):
     NETWORK_UNREACHABLE = 0
@@ -79,11 +90,13 @@ class ICMP_DEST_UNREACH(Enum):
     HOST_PRECEDENCE = 14
     PRECEDENCE_CUTOFF = 15
 
+
 class ICMP_REDIRECT(Enum):
     NETWORK = 0
     HOST = 1
     TOS_NETWORK = 2
     TOS_HOST = 3
+
 
 ICMP_TYPE_CODE = {
     0: lambda x: "",
@@ -93,143 +106,106 @@ ICMP_TYPE_CODE = {
 }
 
 
-class Packet(BaseRig):
+class Packet(BaseMonitor):
     """Monitor network traffic.
 
-    Detects network traffic matching a defined IP address, port, 
+    Detects network traffic matching a defined IP address, port,
     set of TCP flags or an ICMP code.
     """
 
-    parser_description = 'Monitor network traffic.'
+    monitor_name = 'packet'
 
-    def set_parser_options(self, parser):
-
-        parser.add_argument('--ifname', type=str,
-                            help='Match network interface')
-
-        parser.add_argument('--srcmac', type=str,
-                            help='Match source MAC address')
-
-        parser.add_argument('--dstmac', type=str,
-                            help='Match destination MAC address')
-
-        parser.add_argument('--srcip', type=str,
-                            help='Match source IP address')
-
-        parser.add_argument('--dstip', type=str,
-                            help='Match destination IP address')
-
-        parser.add_argument('--srcport', type=int,
-                            help='Match source port/protocol')
-
-        parser.add_argument('--dstport', type=int,
-                            help='Match destination port/protocol')
-
-        parser.add_argument('--tcpflags', type=str,
-                            help='Match TCP flags')
-
-        parser.add_argument('--icmptype', type=str,
-                            help='Match ICMP type')
-
-        parser.add_argument('--payload', type=str,
-                            help='Match any payload string using a regular expression')
-
-
-        parser.add_argument('--any', action="store_true",
-                            help='Any parameter triggers (default: all params)')
-
-    @property
-    def watching(self):
-        s = "Network packets"
-        ifname = self.get_option("ifname")
-        if ifname:
-            s += f" on interface {ifname}"
-        return s
-
-    @property
-    def trigger(self):
-        triggers = []
-        op_str = " or " if self.get_option('any') else " and "
-        for x, v in self._must_match.items():
-            triggers.append(f'{x} is {v}')
-
-        return op_str.join(triggers)
-
-    def setup(self):
+    def configure(self, interface, srcmac=None, dstmac=None, srcip=None,
+                  dstip=None, srcport=None, dstport=None, tcpflags=None,
+                  icmptype=None, payload=None, trigger_any=False):
+        """
+        :param interface: The network interface to listen on
+        :param srcmac: Match this source mac address
+        :param dstmac: Match this destination mac address
+        :param srcip: Match this source IP address
+        :param dstip: Match this destination IP address
+        :param srcport: Match this source port
+        :param dstport: Match this destination port
+        :param tcpflags: Match these TCP protocol flags
+        :param icmptype: Match this ICMP type
+        :param payload: Match a clear text payload using a regex
+        :param trigger_any: Trigger this monitor when any provided filter
+                            matches, otherwise require all filters to match
+        """
 
         self._match_ifname = None
+        self.trigger_any = trigger_any
 
         self._must_match = {
-            'srcmac': self.get_option('srcmac'),
-            'dstmac': self.get_option('dstmac'),
-            'srcip': self.get_option('srcip'),
-            'dstip': self.get_option('dstip'),
-            'srcport': self.get_option('srcport'),
-            'dstport': self.get_option('dstport'),
+            'srcmac': srcmac,
+            'dstmac': dstmac,
+            'srcip': srcip,
+            'dstip': dstip,
+            'srcport': srcport,
+            'dstport': dstport,
         }
 
         # Build a TCP_FLAGS instance based on the provided flags
-        tcpflags_str = self.get_option('tcpflags')
-        if tcpflags_str:
-            tcpflags_int = sum([ getattr(TCP_FLAGS, x.upper()) \
-                                for x in tcpflags_str.split('|') ])
+        if tcpflags:
+            if isinstance(tcpflags, str):
+                tcpflags = [tcpflags]
+            elif not isinstance(tcpflags, list):
+                raise Exception(
+                    "'tcpflags' must be provided as string or list"
+                )
+            tcpflags_int = sum([
+                getattr(TCP_FLAGS, x.upper()) for x in tcpflags
+            ])
 
             self._must_match['tcpflags'] = TCP_FLAGS(tcpflags_int)
 
-
         # Get ICMP_TYPES instance from the provided icmp type name
-        icmptype_str = self.get_option('icmptype')
+        icmptype_str = icmptype
         if icmptype_str:
             self._must_match['icmptype'] = \
-                getattr(ICMP_TYPES, icmptype_str.upper().replace('-', '_'), None)
+                getattr(
+                    ICMP_TYPES, icmptype_str.upper().replace('-', '_'),
+                    None
+                )
 
         # Fail if the network interface doesn't exist on the system.
-        ifname = self.get_option('ifname')
-        if ifname:
+        if interface:
             try:
-                if_nametoindex(ifname)
-                self._match_ifname = ifname
+                if_nametoindex(interface)
+                self._match_ifname = interface
             except OSError:
-                raise CannotConfigureRigError(f"Interface \"{ifname}\" doesn't exist.")
+                raise Exception(f"Interface '{interface}' does not exist")
 
         # Build a comparable object if a payload regex is provided
-        payload = self.get_option('payload')
         if payload:
             self._must_match['payload'] = PayloadMatch(payload)
 
         # Remove all attributes that have no value
-        self._must_match = { k: v for k, v in self._must_match.items() if v }
+        self._must_match = {k: v for k, v in self._must_match.items() if v}
 
         # No filters provided
         if not self._must_match:
-            raise CannotConfigureRigError("Must specify at least one filter")
+            raise Exception("Must specify at least one filter")
 
-        self.add_watcher_thread(target=self._read_from_socket, args=())
-
+        self.add_monitor_thread(self._read_from_socket, ())
 
     @staticmethod
     def _strmac(mac):
-        return ":".join([ f"{i:02x}" for i in mac])
+        return ":".join([f"{i:02x}" for i in mac])
 
     def _pkt_matches(self, pkt_attrs):
-
         matching_keys = {}
-
         for k, v in self._must_match.items():
-
-            if not k in pkt_attrs:
+            if k not in pkt_attrs:
                 continue
-
             # Match tcpflags if any of the provided flags are set
             if isinstance(v, TCP_FLAGS) and v & pkt_attrs[k] != 0:
                 matching_keys[k] = v
-
             # For all other keys we just compare them
             elif pkt_attrs[k] == v:
                 matching_keys[k] = v
 
-
-        if self.get_option("any"):
+        if self.trigger_any:
             if len(matching_keys) > 0:
                 return matching_keys
             else:
@@ -243,7 +219,6 @@ class Packet(BaseRig):
                 return None
 
     def _read_from_socket(self):
-
         sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))
 
         while True:
@@ -258,30 +233,28 @@ class Packet(BaseRig):
             if self._match_ifname and self._match_ifname != iface:
                 continue
 
-            if ethtype_info != ETH_IPV4: # Only IPv4
+            if ethtype_info != ETH_IPV4:  # Only IPv4
                 continue
 
             # L2
-
             eth_src = self._strmac(eth[:6])
             eth_dst = self._strmac(eth[6:12])
-            #eth_type = struct.unpack("!H", eth[12:14])[0]
+            # eth_type = struct.unpack("!H", eth[12:14])[0]
 
             pkt_attrs["srcmac"] = eth_src
             pkt_attrs["dstmac"] = eth_dst
 
             # L3
-
             ip = eth[14:]
 
             ip_ver, ip_hdrlen = divmod(ip[0], 16)
-            ip_hdrlen *= 4 # 32bit incrs
+            ip_hdrlen *= 4  # 32bit incrs
 
             if ip_ver != 4:
                 continue
 
             ip_pktlen, ip_id, ip_flags, ip_ttl, ip_proto, ip_cksum = \
-                    unpack("!HHHBBH", ip[2:12])
+                unpack("!HHHBBH", ip[2:12])
 
             ip_src = inet_ntoa(ip[12:16])
             ip_dst = inet_ntoa(ip[16:20])
@@ -290,13 +263,12 @@ class Packet(BaseRig):
             pkt_attrs['dstip'] = ip_dst
 
             # L4
-
             if ip_proto == IPPROTO_TCP:
                 tcp = ip[ip_hdrlen:]
                 tcp_src, tcp_dst, tcp_seq, tcp_ack, tcp_hdrlen, tcp_flags, \
-                        tcp_win, tcp_chksum = unpack("!HHLLBBHH", tcp[0:18])
+                    tcp_win, tcp_chksum = unpack("!HHLLBBHH", tcp[0:18])
 
-                tcp_hdrlen = (tcp_hdrlen >> 4) * 4 # 32bit incrs
+                tcp_hdrlen = (tcp_hdrlen >> 4) * 4  # 32bit incrs
                 tcp_flags = TCP_FLAGS(tcp_flags)
 
                 pkt_attrs['srcport'] = tcp_src
@@ -306,9 +278,11 @@ class Packet(BaseRig):
 
                 payload = tcp[tcp_hdrlen:]
 
-                pkt_str = (f"{ip_src:>15s}:{tcp_src:<5d} ({eth_src}) -> "
-                             f"{ip_dst:>15s}:{tcp_dst:<5d} ({eth_dst}) "
-                             f"{str(tcp_flags).replace('TCP_FLAGS.', '')} {payload}")
+                pkt_str = (
+                    f"{ip_src:>15s}:{tcp_src:<5d} ({eth_src}) -> "
+                    f"{ip_dst:>15s}:{tcp_dst:<5d} ({eth_dst}) "
+                    f"{str(tcp_flags).replace('TCP_FLAGS.', '')} {payload}"
+                )
 
             elif ip_proto == IPPROTO_UDP:
                 udp = ip[ip_hdrlen:]
@@ -317,15 +291,17 @@ class Packet(BaseRig):
                 pkt_attrs['srcport'] = udp_src
                 pkt_attrs['dstport'] = udp_dst
 
-                payload = udp[8:] # UDP header is fixed 8 bytes
+                payload = udp[8:]  # UDP header is fixed 8 bytes
 
-                pkt_str = (f"{ip_src:>15s}:{udp_src:<5d} ({eth_src}) -> "
-                             f"{ip_dst:>15s}:{udp_dst:<5d} ({eth_dst}) {payload}")
+                pkt_str = (
+                    f"{ip_src:>15s}:{udp_src:<5d} ({eth_src}) -> "
+                    f"{ip_dst:>15s}:{udp_dst:<5d} ({eth_dst}) {payload}"
+                )
 
             elif ip_proto == IPPROTO_ICMP:
                 icmp = ip[ip_hdrlen:]
                 icmp_type, icmp_code, icmp_cksum, icmp_id, icmp_seq = \
-                        unpack("!BBHHH", icmp[:8])
+                    unpack("!BBHHH", icmp[:8])
 
                 try:
                     icmp_type = ICMP_TYPES(icmp_type)
@@ -334,17 +310,21 @@ class Packet(BaseRig):
                 except ValueError:
                     pass
 
-                pkt_str = (f"{ip_src:>15s} ({eth_src}) -> "
-                             f"{ip_dst:>15s} ({eth_dst}) "
-                             f"ICMP {icmp_type.name}")
-
+                pkt_str = (
+                    f"{ip_src:>15s} ({eth_src}) -> "
+                    f"{ip_dst:>15s} ({eth_dst}) "
+                    f"ICMP {icmp_type.name}"
+                )
+                self.logger.debug(pkt_str)
 
             pkt_attrs['payload'] = payload
 
             matching_keys = self._pkt_matches(pkt_attrs)
             if matching_keys:
-                match_str = " and ".join([ f"{k} {v}" for k, v in matching_keys.items() ])
-                self.log_info(f"Packet matching {match_str} found: {pkt_str}")
+                match_str = " and ".join([
+                    f"{k} {v}" for k, v in matching_keys.items()
+                ])
+                self.logger.info(
+                    f"Packet matching {match_str} found: {pkt_str}"
+                )
                 return True
-
-
