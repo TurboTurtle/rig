ce1ed631ac405e6bbca09de22e7a76d16948831e
Jake Hunsaker <jhunsake@redhat.com>
2023-02-08 16:23:29 -0500

    [destroy] Re-implement the `destroy` command
    Re-implements the `destroy` command for the new design so that users can
    run `rig destroy` and terminate a rig without triggering its actions or
    generating a rig archive.
    
    This is the first command to make sure of socket communication with
    rigs, and as such makes changes to `BaseRig` such as flipping us over to
    using a `multiprocessing.connection` Listener rather than directly
    leveraging and manipulating the socket ourselves.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- rigging/commands/destroy.py
+++ rigging/commands/destroy.py
@@ -8,16 +8,61 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
+import sys
+import os
+
 from rigging.commands import RigCmd
+from rigging.connection import RigConnection, RIG_SOCK_DIR
+from rigging.exceptions import DeadRigError
 
 
 class DestroyCmd(RigCmd):
     """
-    Forcibly destroys a rig.
+    Terminate a rig without triggering its actions, or generating an archive
+    containing any compiled data until now.
+
+    This command may be called for a single rig, like so:
+
+    rig destroy my-test-rig
+
+    Or for multiple rigs:
+
+    rig destroy my-test-rig my-other-rig anotherrig
     """
 
     parser_description = 'Terminate a rig without triggering its actions'
 
     @classmethod
     def add_parser_options(cls, parser):
-        parser.add_argument('-i', '--id', help='The ID of the rig to destroy')
+        parser.add_argument('rig_id', nargs='+',
+                            help='The ID or name of the rig(s) to destroy')
+        parser.add_argument('--force', action='store_true', default=False,
+                            help='Force remove a dead rig')
+
+    def execute(self):
+        for target in self.options['rig_id']:
+            try:
+                self._run_destroy(target)
+                sys.stdout.write(f"Rig '{target}' destroyed\n")
+            except Exception as err:
+                # don't stop iteration due to one bad attempt
+                sys.stdout.write(f"{err}\n")
+
+    def _run_destroy(self, target):
+        try:
+            conn = RigConnection(target)
+            ret = conn.destroy_rig()
+            if not ret['success']:
+                raise Exception(f"Failed to destroy rig: {ret['result']}")
+        except OSError:
+            raise Exception(f"No such rig: {target}")
+        except DeadRigError:
+            if not self.options['force']:
+                raise
+            else:
+                try:
+                    os.unlink(os.path.join(RIG_SOCK_DIR, target))
+                except Exception as err:
+                    raise Exception(
+                        f"Could not remove dead rig '{target}': {err}"
+                    )
