440889de2b4adcbfdf6275d989c5ef958fbe9f27
Juanma Sanchez <juasanch@redhat.com>
2023-02-20 08:31:35 +0100

    [list] Adding very basic initial version of `list` command.
    This is just for reference about how to find all running
    rigs by checking the current list of running dbus services.
    
    For now it just spawns a session bus, tries to find all
    rigs currently running, and prints their names.
    
    Future versions would create a `RigDBusConnection` per running rig
    and gather basic information from them once the `status` command
    is implemented.
    
    Signed-off-by: Juanma Sanchez <juasanch@redhat.com>

---
+++ rigging/commands/list.py
@@ -0,0 +1,12 @@
+import dbus
+
+from rigging.commands import RigCmd
+
+class ListCmd(RigCmd):
+
+    def execute(self):
+        bus = dbus.SessionBus()
+        for service_name in bus.list_names():
+            if service_name.startswith("com.redhat.Rig."):
+                rig_id = service_name.split(".")[-1]
+                print(rig_id)
