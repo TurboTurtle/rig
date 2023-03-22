87ab9987499025bbbe4f6d4d44dbafef5e68cf6b
Juanma Sanchez <juasanch@redhat.com>
2023-03-22 11:09:24 +0100

    Bring license header back
    Signed-off-by: Juanma Sanchez <juasanch@redhat.com>

--- rigging/connection.py
+++ rigging/connection.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2023 Red Hat, Inc., Jake Hunsaker <jhunsake@redhat.com>
+#
+# This file is part of the rig project: https://github.com/TurboTurtle/rig
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+
 import dbus
 import dbus.service
 import dbus.mainloop.glib
